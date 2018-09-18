a6400e190b84645cd869814de521dee288d44b3e
Chii Aik Fang <chiiaik@tinkerstuff.com>
2018-09-18 14:57:11 +0800

    Add support for region configuration

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 const isEmpty = require('lodash.isempty');
 const AWS = require('aws-sdk');
 
-module.exports = {
-    dispatch: function (fnNameOrArn, invocationType, payload) {
-        if (isEmpty(fnNameOrArn)) {
-            return Promise.reject(new Error('missing lambda function name or ARN'));
-        }
-        if (isEmpty(invocationType) && isEmpty(payload)) {
-            return Promise.reject(new Error('missing payload'));
-        }
-        let aType, aPayload;
-        if (invocationType && payload) {
-            aType = invocationType;
-            aPayload = payload;
-        } else {
-            aType = 'Event';
-            aPayload = invocationType;
-        }
-        return new Promise(function(resolve, reject) {
-            let params = {
-                FunctionName: fnNameOrArn,
-                InvocationType: aType,
-                Payload: JSON.stringify(aPayload),
-            };
-            new AWS.Lambda().invoke(params, function(error, data) {
-                if (error) {
-                    return reject(error);
-                }
-                resolve(data);
-            });
-        });
-    }
+function LambdaDispatcher(config) {
+    this.config = config;
 }
+
+LambdaDispatcher.prototype.dispatch = function (fnNameOrArn, invocationType, payload) {
+    if (isEmpty(fnNameOrArn)) {
+        return Promise.reject(new Error('missing lambda function name or ARN'));
+    }
+    if (isEmpty(invocationType) && isEmpty(payload)) {
+        return Promise.reject(new Error('missing payload'));
+    }
+    let self = this;
+    let aType, aPayload;
+    if (invocationType && payload) {
+        aType = invocationType;
+        aPayload = payload;
+    } else {
+        aType = 'Event';
+        aPayload = invocationType;
+    }
+    return new Promise(function(resolve, reject) {
+        let params = {
+            FunctionName: fnNameOrArn,
+            InvocationType: aType,
+            Payload: JSON.stringify(aPayload),
+        };
+        new AWS.Lambda(self.config).invoke(params, function(error, data) {
+            if (error) {
+                return reject(error);
+            }
+            resolve(data);
+        });
+    });
+}
+
+module.exports = LambdaDispatcher;
